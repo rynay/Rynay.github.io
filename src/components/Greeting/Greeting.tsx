a2c982602870e7406e0eaeed630551db65b6b931
Rynay <feirs911@gmail.com>
2021-08-08 12:37:27 +0300

    about layout

--- src/components/Greeting/Greeting.tsx
+++ src/components/Greeting/Greeting.tsx
@@ -1,5 +1,4 @@
 import { data } from '../../data'
-import s from './Greeting.module.scss'
 
 type PropTypes = {
   language: keyof typeof data['languages']
@@ -7,7 +6,7 @@ type PropTypes = {
 
 export const Greeting = ({ language }: PropTypes) => {
   return (
-    <section className={s.section}>
+    <section>
       <h1>{data.headings.greeting[language]}</h1>
     </section>
   )
--- src/components/Last7DaysActivity/Last7DaysActivity.tsx
+++ src/components/Last7DaysActivity/Last7DaysActivity.tsx
@@ -8,39 +8,33 @@ type PropTypes = {
 }
 
 export const Last7DaysActivity = ({ theme, language }: PropTypes) => {
-  const [isLightShown, setIsLightShown] = useState<boolean>()
-  const [isDarkShown, setIsDarkShown] = useState<boolean>()
-
-  useEffect(() => {
-    setIsLightShown(theme === 'light')
-    setIsDarkShown(theme === 'dark')
-  }, [theme])
-
   return (
     <section>
       <h2>{data.headings.activity[language]}:</h2>
-      <figure
-        className={`${s.lastActivity__chartsContainer} ${
-          !isLightShown ? s.lastActivity__chartsContainer_hidden : ''
-        }`}>
-        <embed
-          width={'50%'}
-          src="https://wakatime.com/share/@rynay/63f497a2-e009-4c3d-be34-5632b9bcb701.svg"></embed>
-        <embed
-          width={'50%'}
-          src="https://wakatime.com/share/@rynay/ed7940e3-4323-43b8-a058-1757ef30ac47.svg"></embed>
-      </figure>
-      <figure
-        className={`${s.lastActivity__chartsContainer} ${
-          !isDarkShown ? s.lastActivity__chartsContainer_hidden : ''
-        }`}>
-        <embed
-          width={'50%'}
-          src="https://wakatime.com/share/@rynay/2a01873b-3f57-47e8-9495-207f740be9cf.svg"></embed>
-        <embed
-          width={'50%'}
-          src="https://wakatime.com/share/@rynay/41168743-ba68-4234-9062-6ea94d26b550.svg"></embed>
-      </figure>
+      <div className={s.lastActivity__content}>
+        <figure
+          className={`${s.lastActivity__chartsContainer} ${
+            theme === 'light' ? '' : s.lastActivity__chartsContainer_hidden
+          }`}>
+          <embed
+            width={'50%'}
+            src="https://wakatime.com/share/@rynay/63f497a2-e009-4c3d-be34-5632b9bcb701.svg"></embed>
+          <embed
+            width={'50%'}
+            src="https://wakatime.com/share/@rynay/ed7940e3-4323-43b8-a058-1757ef30ac47.svg"></embed>
+        </figure>
+        <figure
+          className={`${s.lastActivity__chartsContainer} ${
+            theme === 'dark' ? '' : s.lastActivity__chartsContainer_hidden
+          }`}>
+          <embed
+            width={'50%'}
+            src="https://wakatime.com/share/@rynay/2a01873b-3f57-47e8-9495-207f740be9cf.svg"></embed>
+          <embed
+            width={'50%'}
+            src="https://wakatime.com/share/@rynay/41168743-ba68-4234-9062-6ea94d26b550.svg"></embed>
+        </figure>
+      </div>
     </section>
   )
 }
